698d087d448143cf2f51f2da801cea49855adf85
Vijay Stroup <vijaystroup@gmail.com>
2020-02-12 16:48:37 -0500

    documentation

--- ml/data_builder.py
+++ ml/data_builder.py
@@ -1,18 +1,37 @@
+""" The data_builder module contains the class DataBuilder which is used to
+save all our raw data to a multi-dimensional list we can load it to train the
+neural network.
+"""
+
 import os
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
+
 class DataBuilder:
+    """ The DataBuilder class is responsible for going through the data files
+    and then saving a list as numpy file with the image and its vector
+    classifier. In addition, a count is supplied to see how much data we have.
+    """
+
     def __init__(self):
+        """ The __init__ function contains variables we will use for saving our
+        data. The LABELS variable indicates when using np.eye(2), vector <1, 0>
+        will equate to data/hot whereas vector <0, 1> will equate to data/not_hot.
+        """
         self.HOT = '../data/hot'
         self.NOT_HOT = '../data/not_hot'
-        self.LABELS = {self.HOT: 1, self.NOT_HOT: 0}
+        self.LABELS = {self.HOT: 0, self.NOT_HOT: 1}
         self.data = []
         self.hot_count = 0
         self.not_hot_count = 0
     
     def generator(self):
+        """ The generator method opens data, and saves it to a list with their
+        proper vector. Then, we shuffle our data so we increase randomness from
+        the ai. Finally, we save the list as a numpy file and print our metrics.
+        """
         for label in self.LABELS:
             for f in tqdm(os.listdir(label)):
                 try:
@@ -31,7 +50,8 @@ class DataBuilder:
         
         np.random.shuffle(self.data)
         np.save('data.npy', self.data)
-        print(f'Hotties: {self.hot_count}\tNot Hotties: {self.not_hot_count}')
+        print(f'Hotties: {self.hot_count}\tNot Hotties: {self.not_hot_count}\n'
+              f'Total: {self.hot_count + self.not_hot_count}')
 
 
 if __name__ == '__main__':
