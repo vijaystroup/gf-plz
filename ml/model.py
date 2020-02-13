1cd6603a0df817891aa739a225d868b4df52b34b
Vijay Stroup <vijaystroup@gmail.com>
2020-02-12 20:12:28 -0500

    model fix

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
 
 class Model(nn.Module):
@@ -12,15 +11,18 @@ class Model(nn.Module):
         self.conv2 = nn.Conv2d(64, 128, 10)
         self.conv3 = nn.Conv2d(128, 256, 10)
 
+        x = torch.randn(800, 640).view(-1, 1, 800, 640)
+
         x = F.max_pool2d(F.relu(self.conv1(x)), (5, 5))
         x = F.max_pool2d(F.relu(self.conv2(x)), (5, 5))
         x = F.max_pool2d(F.relu(self.conv3(x)), (5, 5))
 
-        self.fc1 = nn.Linear(torch.flatten(x), 512)
+        # 2048 is flattened tensor of [1, 256, 4, 2] from previous output
+        self.fc1 = nn.Linear(2048, 512)
         self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
-        x = x.view(-1, torch.flatten(x))
+        x = x.view(-1, 2048)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.softmax(x, dim=1)
